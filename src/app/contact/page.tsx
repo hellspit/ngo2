1c0f15d15223bdbb0b91a6f54e3b928c6f1b406c
Anuj <ece22123@iiitkalyani.ac.in>
2025-06-03 16:56:23 +0530

    added link social

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -139,7 +139,7 @@ export default function JoinUsPage() {
                 <Phone size={24} />
               </div>
               <h3>Phone</h3>
-              <p>+91 9454876659</p>
+              <p>+91 6306543085</p>
               <p>Mon-Fri, 9:00 AM - 5:00 PM</p>
             </div>
 
@@ -148,7 +148,7 @@ export default function JoinUsPage() {
                 <Mail size={24} />
               </div>
               <h3>Email</h3>
-              <p>space@dansf.org</p>
+              <p>spacefoundation@dansf.org</p>
               <p>We'll respond within 24 hours</p>
             </div>
 
@@ -334,13 +334,13 @@ export default function JoinUsPage() {
                   <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                   <polyline points="22,6 12,13 2,6"></polyline>
                 </svg>
-                <span>space@dansf.org</span>
+                <span>spacefoundation@dansf.org</span>
               </li>
               <li>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                 </svg>
-                <span>+91 9454876659</span>
+                <span>+91 6306543085</span>
               </li>
               <li>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -156,7 +156,7 @@ export default function Navbar() {
     <h1><span className="highlight">Join</span> Our Team</h1>
     <h2>to make<br />someone's life<br />better</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum enim nobis dolorum maiores repellendus sunt alias officiis blanditiis quisquam. In.</p>
-    <button className="join-btn">Join us</button>
+    <Link href="/contact" className="join-btn" style={{ textDecoration: 'none' }}>Join us</Link>
   </div>
   <div className="join-right">
     <img src="/earth.gif" alt="Earth Background" className="earth-img" />
@@ -315,13 +315,13 @@ export default function Navbar() {
             <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
             <polyline points="22,6 12,13 2,6"></polyline>
           </svg>
-          <span>space@dansf.org</span>
+          <span>spacefoundation@dansf.org</span>
         </li>
         <li>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
           </svg>
-          <span>+91 9454876659</span>
+          <span>+91 6306543085</span>
         </li>
         <li>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
